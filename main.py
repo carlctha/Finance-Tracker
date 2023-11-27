f4b70503f0a7ff2741a50497bd2f24c6ce39dc83
Carl <carlctha1@gmail.com>
2023-11-27 23:57:07 +0100

    Implemented basic google sheets api

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import csv
+import gspread
 from parse_csv import parse_csv_data
-
 
 MONTH = "november"
 
 file = f"swedbank_{MONTH}.csv"
-print(file)
-
+transactions = []
 
 with open(file, mode="r", encoding="utf8") as csv_f:
     reader = csv.reader(csv_f)
     for row in reader:
-        print(row)
+        date = row[0]
+        name = row[1]
+        amount = float(row[2])
+        transaction = ((date, name, amount))
+        transactions.append(transaction)
 
 
 if __name__ == "__main__":
-    parse_csv_data(file)
+    #parse_csv_data(file)
+    gc = gspread.service_account()
+    sh = gc.open("Finance")
